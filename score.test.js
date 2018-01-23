6bfc48b49b5f0430bd388cffa6a1b9514fdb6b43
Shoaib <ahamedshoaibk@gmail.com>
2018-01-23 10:35:35 +0530

    added first test suite for not spares and strikes

--- score.test.js
+++ score.test.js
@@ -1,5 +1,16 @@
-const score = require("./score")
+const score = require('./score');
 
+describe('all throws are not spares or strikes', () => {
+  test('given all throws as 0 return 0 (all gutter balls)', () => {
+    const input = [];
+    const output = 0;
+    expect(score(input)).toBe(output);
+  });
 
-
+  test('given all throws as 1 return 20 (20 throws of 1)', () => {
+    const input = [];
+    const output = 0;
+    expect(score(input)).toBe(output);
+  });
+});
 
