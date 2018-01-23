23ed7e2dd35ec760c716e63801ea1ded0eef2424
Shoaib <ahamedshoaibk@gmail.com>
2018-01-23 12:01:27 +0530

    added comments

--- score.js
+++ score.js
@@ -1,21 +1,25 @@
 const calculateScore = (rolls) => {
   const numRolls = rolls.length;
 
-  let rollCount = 0;
-  let score = 0;
+  let rollCount = 0; // count of current roll
+  let score = 0; // score not including any bonus
 
-  let frameNumber = 0;
-  let frameCount = 0;
-  let frameScore = 0;
+  let frameNumber = 0; // number of current frame
+  let frameCount = 0; // count of current roll in frame
+  let frameScore = 0; // score of current frame
 
+  // is current frame a spare or strike
   let isSpare = false;
   let isStrike = false;
 
+  // bonus got from spares and strikes
   let spareBonus = 0;
   let strikeBonus = 0;
 
   for (rollCount = 0; rollCount < numRolls; rollCount += 1) {
+    // total frames in a game of bowling is 10
     if (frameNumber < 10) {
+      // increment frame number and reset frame variables if new frame
       if (frameCount === 2 || frameScore === 10) {
         frameNumber += 1;
         frameCount = 0;
@@ -25,12 +29,14 @@ const calculateScore = (rolls) => {
       score += rolls[rollCount];
       frameScore += rolls[rollCount];
 
+      // check if current frame is a strike or spare
       if (frameScore === 10 && frameCount === 0) {
         isStrike = true;
       } else if (frameScore === 10) {
         isSpare = true;
       }
 
+      // calculate and add bonus from current frame
       if (isStrike) {
         strikeBonus += rolls[rollCount + 1] + rolls[rollCount + 2];
         isStrike = false;
